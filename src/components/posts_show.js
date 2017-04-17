81a8499bfe317918a5e7da1ffe88b13553aceac6
Eleutherado <emiliov432@gmail.com>
2017-04-17 01:46:32 -0400

    added content for viewing posts

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -8,9 +8,28 @@ class PostsShow extends Component {
     this.props.viewPost(this.props.params.id)
   }
   render(){
-    return <div>Show post {this.props.params.id}</div>;
+
+    const { story } = this.props;
+
+    if(!story){
+      return <div>loading...</div>
+
+    }
+    return(
+      <div>
+        <h2 className="center-align light-blue-text text-darken-4">{story.title}</h2>
+        <h6 className="left-align">Categories: {story.categories}</h6>
+        <p className="blue-grey-text text-darken-1">{story.content}</p>
+      </div>
+    );
   }
 
 }
 
-export default connect(null, {viewPost})(PostsShow);
+function mapStateToProps(state){
+  //using the name story to illustrate which part of the state corresponds
+  // to this.props vs state.props (!=)
+  return { story: state.posts.post }
+}
+
+export default connect(mapStateToProps, {viewPost})(PostsShow);
